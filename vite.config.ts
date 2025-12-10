76340d65de3fd5e718a1629ff95116aec0aa2b4f
Montanahub <tapsobasimonwendzodo@gmail.com>
2025-12-10 09:42:06 +0000

    j

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+
+  plugins: [
+    sitemap({
+      hostname: 'https://https://tapsobasimonwendzodoportfoliocv.onrender.com'
+    })
+  ]
+
+  const env = loadEnv(mode, '.', '');
     return {
       server: {
         port: 3000,
@@ -20,4 +28,8 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
+
+
+
+
 });
